e9d2dc53edb38b419390afe5ab18ab22926f2a7b
Dima Groisman <dima.groisman@aquasec.com>
2023-12-25 13:09:47 +0200

    change color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,40 +1,46 @@
 return {
-  { "EdenEast/nightfox.nvim" },
   {
     "sainnhe/sonokai",
     init = function() -- init function runs before the plugin is loaded
-      vim.g.sonokai_style = "atlantis"
-      vim.g.transparent_background = 2
+      vim.g.sonokai_style = "andromeda"
+      vim.g.transparent_background = 0
       vim.g.sonokai_dim_inactive_windows = 0
       vim.g.sonokai_better_performance = 1
+      vim.g.sonokai_disable_terminal_colors = 1
     end,
   },
-
   {
     "folke/tokyonight.nvim",
+    lazy = false,
     opts = {
-      transparent = true,
+      style = "dark",
+      terminal_colors = false,
+      transparent = false,
       styles = {
-        sidebars = "transparent",
-        floats = "transparent",
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
       },
     },
   },
-
   {
-    "craftzdog/solarized-osaka.nvim",
+    "catppuccin/nvim",
     lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = false,
-    },
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup({
+        flavour = "macchiato",
+        transparent_background = false,
+        term_colors = false,
+      })
+    end,
   },
 
   -- Configure LazyVim to load a theme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "solarized-osaka",
+      colorscheme = "sonokai",
     },
   },
 }
