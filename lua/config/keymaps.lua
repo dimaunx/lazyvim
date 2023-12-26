5114a37534034d9a7ea24493b9776a963933cc77
Dima Groisman <dima.groisman@aquasec.com>
2023-12-26 15:18:13 +0200

    add go.nvim keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,3 +20,12 @@ keymap.set("n", "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical name=t
 -- stylua: ignore
 keymap.set("n", "<leader>th", "<cmd>ToggleTerm size=20 direction=horizontal name=term<cr>", { desc = "Horizontal terminal" })
 keymap.set("n", "<leader>tt", "<cmd>ToggleTerm direction=float name=term<cr>", { desc = "Float terminal" })
+
+-- GO https://github.com/ray-x/go.nvim
+keymap.set("n", "<leader>cgc", "<cmd>GoCmt<cr>", { desc = "Go gen function comment" })
+keymap.set("n", "<leader>cgs", "<cmd>GoFillStruct<cr>", { desc = "Go fill struct" })
+keymap.set("n", "<leader>cga", "<cmd>GoAddTag<cr>", { desc = "Go add struct tags" })
+keymap.set("n", "<leader>cgr", "<cmd>GoRmTag<cr>", { desc = "Go remove struct tags" })
+keymap.set("n", "<leader>cgi", "<cmd>GoIfErr<cr>", { desc = "Go add if err" })
+keymap.set("n", "<leader>cgj", "<cmd>GoJson2Struct Gen<cr>", { desc = "Go gen struct from json" })
+keymap.set("n", "<leader>cgt", "<cmd>GoAddTest<cr>", { desc = "Go gen test for a function" })
