c043b1966de5ae5ec86931c2241bffbede8195cc
Dima Groisman <dima.groisman@aquasec.com>
2024-01-02 21:09:49 +0200

    add local-highlight

---
+++ lua/plugins/local-highlight.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "tzachar/local-highlight.nvim",
+    config = function()
+      require("local-highlight").setup({
+        file_types = { "python", "go" }, -- If this is given only attach to this
+        -- OR attach to every filetype except:
+        disable_file_types = { "tex" },
+        hlgroup = "Search",
+        cw_hlgroup = nil,
+        -- Whether to display highlights in INSERT mode or not
+        insert_mode = false,
+      })
+    end,
+  },
+}
