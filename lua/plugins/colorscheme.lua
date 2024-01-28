769ae4501c2ff2969546e7f7ac9fa2c2f21f1df4
Dima Groisman <dima.groisman@aquasec.com>
2024-01-28 14:59:02 +0200

    feat: add tmux and alacritty configs

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -20,10 +20,10 @@ return {
         style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
         light_style = "day", -- The theme is used when the background is set to light
         transparent = false, -- Enable this to disable setting the background color
-        terminal_colors = false, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
         styles = {
           -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "transparent", -- style for sidebars, see below
+          sidebars = "dark", -- style for sidebars, see below
           floats = "dark", -- style for floating windows
         },
         sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
