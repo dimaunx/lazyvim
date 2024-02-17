135290e4ad82193f5a5f4f75e327c6662b58d148
Dima Groisman <dima.groisman@aquasec.com>
2024-02-17 14:00:14 +0200

    feat: update go.nvim plugin

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -7,7 +7,50 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
     config = function()
-      require("go").setup()
+      require("go").setup({
+        disable_defaults = false, -- true|false when true set false to all boolean settings and replace all table
+        -- settings with {}
+        go = "go", -- go command, can be go[default] or go1.18beta1
+        goimport = "gopls", -- goimport command, can be gopls[default] or either goimport or golines if need to split long lines
+        fillstruct = "gopls", -- default, can also use fillstruct
+        gofmt = "gofumpt", --gofmt cmd,
+        max_line_len = 128, -- max line length in golines format, Target maximum line length for golines
+        tag_transform = false, -- can be transform option("snakecase", "camelcase", etc) check gomodifytags for details and more options
+        tag_options = "json=omitempty",
+        lsp_inlay_hints = {
+          enable = true,
+          -- hint style, set to 'eol' for end-of-line hints, 'inlay' for inline hints
+          -- inlay only avalible for 0.10.x
+          style = "eol",
+          -- Note: following setup only works for style = 'eol', you do not need to set it for 'inlay'
+          -- Only show inlay hints for the current line
+          only_current_line = false,
+          -- Event which triggers a refersh of the inlay hints.
+          -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
+          -- not that this may cause higher CPU usage.
+          -- This option is only respected when only_current_line and
+          -- autoSetHints both are true.
+          only_current_line_autocmd = "CursorHold",
+          -- whether to show variable name before type hints with the inlay hints or not
+          -- default: false
+          show_variable_name = true,
+          -- prefix for parameter hints
+          parameter_hints_prefix = "󰊕 ",
+          show_parameter_hints = true,
+          -- prefix for all the other hints (type, chaining)
+          other_hints_prefix = "=> ",
+          -- whether to align to the lenght of the longest line in the file
+          max_len_align = false,
+          -- padding from the left if max_len_align is true
+          max_len_align_padding = 1,
+          -- whether to align to the extreme right or not
+          right_align = false,
+          -- padding from the right if right_align is true
+          right_align_padding = 6,
+          -- The color of the hints
+          highlight = "Comment",
+        },
+      })
     end,
     event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
